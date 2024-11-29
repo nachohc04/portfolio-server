6c6d8c33899eeadaf8fce389781fe8b58e0e4ee6
Ignacio <nachohcorrea@gmail.com>
2024-11-29 14:00:31 -0300

    Endpoint to create collaborator fixed

--- collaborators/endpoints.py
+++ collaborators/endpoints.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
-from collaborators.collaborators_model import Collaborator, CollaboratorPublic, CollaboratorCreate
+from collaborators.collaborators_model import CollaboratorPublic, CollaboratorCreate, Collaborator
 from database.db import get_session
 
 collaborator_router = APIRouter(
@@ -10,13 +10,13 @@ collaborator_router = APIRouter(
 @collaborator_router.post(
         "/Collaborators",
         status_code=status.HTTP_201_CREATED,
-        response_model=Collaborator
+        response_model=CollaboratorPublic
 )
 async def create_collaborator(
         collaborator_data: CollaboratorCreate,
         session: Session = Depends(get_session)
 ) -> CollaboratorPublic:
-    collaborator = collaborator(**collaborator_data.model_dump())
+    collaborator = Collaborator(**collaborator_data.model_dump())
     session.add(collaborator)
     session.commit()
     session.refresh(collaborator)
